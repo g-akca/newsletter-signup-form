778684310b928f2e848b26c810cf56da2c55adda
g-akca <gok.akca66@gmail.com>
2026-02-18 18:49:32 +0300

    Make dismiss button work

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const form = document.getElementById("signup-form");
 const email = document.getElementById("email");
 const errorMsg = document.getElementById("error-msg");
+const dismissBtn = document.getElementById("dismiss");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -22,4 +23,10 @@ form.addEventListener("submit", (e) => {
         document.getElementById("success-msg").innerHTML = `A confirmation email has been sent to <strong>${emailValue}</strong>. 
         Please open it and click the button inside to confirm your subscription`;
     }
+});
+
+dismissBtn.addEventListener("click", () => {
+    document.querySelector("main").classList.remove("success");
+    document.getElementById("main-section").classList.remove("hidden");
+    document.getElementById("success-section").classList.add("hidden");
 });
